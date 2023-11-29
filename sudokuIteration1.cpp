dfec15b14b91fe2d78944f61fe6f55315b3485a9
Hannah Cherb <hannahlcherb@gmail.com>
2023-11-29 17:59:35 -0600

    Implements scoring with a base score, mistake penalties, and combos

--- sudokuIteration1.cpp
+++ sudokuIteration1.cpp
@@ -2,11 +2,9 @@
 // Program Description: Sudoku Game :D
 
 
-
 #include<iostream>
 #include<string>
 #include<vector>
-#include<fstream>
 #include "boards.h" // include header file
 
 using namespace std;
@@ -17,8 +15,13 @@ private:
 
     int choiceDifficulty; // difficulty users choose
     int choiceLevel; // level (board)
+    int mistakes;
+    int comboStreak;
+    int score;
+
 
 public:
+
 
     void getChoice()
     {
@@ -42,9 +45,9 @@ public:
             cout << endl << "Choose your level:" << endl;
             // this is where the design could come in!!
 
-            cout << "1 - Rocky Mountain Climb" << endl;
-            cout << "2 - Arctic Circle Plunge" << endl;
-            cout << "3 - Amazon Jungle Dash" << endl;
+            cout << "1 - Grand Canyon Expedition" << endl;
+            cout << "2 - Rocky Mountain Climb" << endl;
+            cout << "3 - Arctic Circle Plunge" << endl;
             cout << "Enter the number corresponding to your choice: ";
             cin >> choiceLevel;
 
@@ -78,8 +81,8 @@ public:
             cout << endl << "Choose your level:" << endl;
             // this is where the design could come in!!
 
-            cout << "1 - Swiss Alps Ascent" << endl;
-            cout << "2 - Grand Canyon Expedition" << endl;
+            cout << "1 - Amazon Jungle Dash" << endl;
+            cout << "2 - Swiss Alps Ascent" << endl;
             cout << "3 - Barrier Reef Dive" << endl;
             cout << "Enter the number corresponding to your choice: ";
             cin >> choiceLevel;
@@ -145,12 +148,10 @@ public:
         else
         {
             // send them back to menu and tell them to try again
-            system("cls"); // better way ? find
+            system("cls");
             getChoice();
         }
     }
-
-
 
 
     void displayBoard(int arr[9][9])
@@ -202,81 +203,69 @@ public:
    // make sure input is correct
     bool correctInput(int row, int col, int num, int arr[9][9])
     {
-        // check row
-        for (int x = 0; x < 9; x++) // go through the columns
+        // Check the row, column, and 3x3 box
+        for (int i = 0; i < 9; i++) 
         {
-            if (arr[row][x] == num) // if the number already appears in another col in the row
+            if (arr[row][i] == num || arr[i][col] == num || arr[(row / 3) * 3 + i / 3][(col / 3) * 3 + i % 3] == num) 
             {
-                return false; // input can NOT go in
+                return false;
             }
         }
-
-        // check column
-        for (int x = 0; x < 9; x++) // go through the rows
-        {
-            if (arr[x][col] == num) // if the number already appears in another row in the col
-            {
-                return false; // input can NOT go in
-            }
-        }
-
-        // check box
-        for (int i = 0; i < 3; i++) // loop through rows
-        {
-            for (int j = 0; j < 3; j++) // loop through columns
-            {
-                // if num is in the box
-                if (arr[i][j] == num)
-                    return false; // input can NOT go in
-            }
-        }
-
-        // if the input follows all the rules
-        return true;   // it shall pass!
-
+        return true;
     }
 
     // check if it is solved ******************************************
-    //bool solved(int arr[9][9])
-    //{
-    //    for (int row = 0; row < 9; row++) // loop through rows
-    //    {
-    //        for (int col = 0; col < 9; col++) // loop through columns
-    //        {
-    //            if (arr[row][col] == 0) // if cell is empty
-    //            {
-    //                for (int num = 1; num <= 9; num++) // try numbers from 1 to 9
-    //                {
-    //                    if (correctInput(row, col, num, arr)) // if the num is in the correct place
-    //                    {
-    //                        arr[row][col] = num; // put the number in the cell
-
-    //                        if (solved(arr)) // check if the board can be solved with this number
-    //                            return true; // if solved, return true
-
-    //                        arr[row][col] = 0; // if the board cannot be solved, reset the cell
-    //                    }
-    //                }
-    //                return false; // if no number works, return false
-    //            }
-    //        }
-    //    }
-    //    return true; // if all cells are filled, return true - it is solved!!
-    //}
-    // ******************************************************************
+    bool solved(int arr[9][9]) 
+    {
+        for (int row = 0; row < 9; row++) 
+        {
+            for (int col = 0; col < 9; col++) 
+            {
+                if (arr[row][col] == 0 || !correctInput(row, col, arr[row][col], arr))
+                {
+                    // If there's an empty cell, the Sudoku is not solved
+                    return false;
+                }
+            }
+        }
+        return true;  
+    }
 
 
+    bool findEmptyCell(int arr[9][9], int& row, int& col) 
+    {
+        for (row = 0; row < 9; row++) 
+        {
+            for (col = 0; col < 9; col++) 
+            {
+                if (arr[row][col] == 0) 
+                {
+                    return true;  // Found an empty cell
+                }
+            }
+        }
+        return false;  // All cells are filled
+    }
 
     void playGame(int arr[9][9])
     {
         // collect choice
         int row, col, num;
+        score = 50; // for pts
+        comboStreak = 0; // for pts
+        mistakes = 0;
+
+
         cout << "To return to the main menu, enter '0 0 0'" << endl << endl;
         cout << "Enter the row, column, and number (1-9) of your Sudoku move (ex: 2 5 2):" << endl;
+        
 
         while (true) // find better way than this
         {
             cin >> row >> col >> num; // get user input
+
+            // show user their score
+            cout << "Current score: " << score << endl;
 
             // if they want to exit to main menu
             if (row == 0 && col == 0 && num == 0)
@@ -285,57 +274,147 @@ public:
                 getChoice();
             }
             // if they enter a number that is not 1-9
-            if (row < 1 || row > 9 || col < 1 || col > 9 || num < 1 || num > 9)
+            else if (row < 1 || row > 9 || col < 1 || col > 9 || num < 1 || num > 9)
             {
                 cout << "Invalid input. Please try again." << endl;
                 continue;
             }
-
-            // Allows numbers to go on the board without logic/being correct
-            if (arr[row - 1][col - 1] == 0 && correctInput(row - 1, col - 1, num, arr))
+            // Allows numbers to go on the board 
+            else if (arr[row - 1][col - 1] == 0 && correctInput(row - 1, col - 1, num, arr))
             {
                 arr[row - 1][col - 1] = num;
                 displayBoard(arr);
-                cout << "Enter the row, column, and number (1-9) of your Sudoku move (ex: 2 5 2):" << endl;
-            }
+                
+                calculateScore();
+                showScore(); 
+                updateComboStreak(true);
 
+                if (solved(arr)) // if the board is solved
+                {
+                    // it returns true? congrats
+                    // it CANNOT be returning true since it is skipping :(
+                    cout << "Congratulations! You solved the puzzle!" << endl;
+                    break;
+                }
+                else
+                {
+                    cout << "Current score: " << score << endl;
+                    cout << "Enter the row, column, and number (1-9) of your Sudoku move (ex: 2 5 2):" << endl;
+                }
+
+            }
             else // if their input is valid but incorrect
             {
                 cout << "Invalid move! Please try again." << endl;
-                // tabulate mistakes 
-                mistakeCounter(choiceLevel);
+                updateComboStreak(false);  // Reset combo streak 
+                mistakeCounter();
+                calculateScore();
+                showScore();
+                cout << "Current score: " << score << endl;
             }
 
-            //if (solved(arr)) // if the board is solved
-            //{
-            //    cout << "Congratulations! You solved the puzzle!" << endl;
-            //    break;
-            //}
         }
     }
 
-    void mistakeCounter(int choiceLevel)
+    void mistakeCounter()
     {
-        static int mistakeCount = 0; // static (no unlimited chances)
-
-        mistakeCount++; 
-        cout << "Mistakes: " << mistakeCount << endl << endl;
+        mistakes++;
+        cout << "Mistakes: " << mistakes << endl << endl;
 
         // EASY LEVEL
-        if (mistakeCount >= 6 && choiceLevel == 1) { // check easy level (6 mistakes)
+        if (mistakes > 6 && choiceLevel == 1)
+        {
             cout << "Game over! You have exceeded the maximum number of mistakes for this difficulty level." << endl << endl;
             exit(0); // Exit the program
         }
         // MEDIUM LEVEL
-        else if (mistakeCount >= 4 && choiceLevel == 2) { // check medium level (4 mistakes)
+        else if (mistakes > 4 && choiceLevel == 2)
+        {
             cout << "Game over! You have exceeded the maximum number of mistakes for this difficulty level." << endl;
             exit(0); // Exit the program
         }
         // HARD LEVEL
-        else if (mistakeCount >= 2 && choiceLevel == 3) { // check hard level (2 mistakes)
+        else if (mistakes > 2 && choiceLevel == 3)
+        {
             cout << "Game over! You have exceeded the maximum number of mistakes for this difficulty level." << endl;
             exit(0); // Exit the program
         }
+    }
+
+    void resetMistakes()
+    {
+        mistakes = 0;
+    }
+
+    void calculateScore() 
+    {
+        // Calculate mistake penalties based on difficulty level
+        int mistakePenalty = 0; 
+
+        if (mistakes>0)
+        {
+            if (choiceLevel == 1)
+            {
+                mistakePenalty = -2;
+            }
+            else if (choiceLevel == 2)
+            {
+                mistakePenalty = -4;
+            }
+            else
+            {
+                mistakePenalty = -10;
+            }
+        }
+
+        // Apply mistake penalty
+        score += mistakePenalty;
+
+        // Calculate combo streak bonus
+        int comboBonus;
+        if (choiceLevel == 1)
+        {
+            comboBonus = 2;
+        }
+        else if (choiceLevel == 2)
+        {
+            comboBonus = 5;
+        }
+        else
+        {
+            comboBonus = 7;
+        }
+
+        // Apply combo streak bonus
+        score += comboBonus * comboStreak;
+
+        resetMistakes();
+
+        if (score < 0)
+        {
+            cout << "Game over! Your score dropped below 0. Better luck next time!" << endl;
+            exit(0); // Exit the program
+        }
+
+    }
+    
+
+    void updateComboStreak(bool correctMove) 
+    {
+        if (correctMove)
+        {
+            comboStreak++;
+        }
+        else
+        {
+            comboStreak = 0;  // Reset combo streak on an incorrect move
+        }
+    }
+
+    void showScore() 
+    {
+        // Display the score
+        cout << "Your score: " << score << endl;
     }
 
     void continueGame()
@@ -361,9 +440,7 @@ public:
         }
     }
 
-
 };
-
 
 
 int main()
